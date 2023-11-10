59f2058b8c90f47b89e73c57c9d1239a9c51183d
victorMenaFlores <menaflores.victor@gmail.com>
2023-11-10 09:11:45 +0100

    añade atributos y contrusctor

--- src/main/java/vehiculos/vehiculo.java
+++ src/main/java/vehiculos/vehiculo.java
@@ -8,6 +8,26 @@ package vehiculos;
  *
  * @author victor
  */
+
+// Definicion de la clase (satic, extens, abstract, etc
 public class vehiculo {
+    
+    // Dentro de una clase hay 2 cosas: Atributos y Metodos
+    // Atributos - características 
+    String matricula;
+    String marca;
+    double velocidad;
+    
+    // Métodos - acciones
+    // Método constructor, se llama igual que la clase, siempre es publico
+    // No devuelve valor, puede tener parámetros
+    
+    public vehiculo(String matricula, String marca, double velocidad){
+        this.matricula=matricula;
+        this.marca=marca;
+        this.velocidad=velocidad;
+        
+        
+    }
     
 }
