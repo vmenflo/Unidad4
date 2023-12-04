f2bbec8a8d934fdee471174ed023d20ce1ff9505
victorMenaFlores <menaflores.victor@gmail.com>
2023-12-04 08:48:59 +0100

    Unidad 4

--- src/main/java/vehiculos/pruebaVehiculo.java
+++ src/main/java/vehiculos/pruebaVehiculo.java
@@ -10,4 +10,22 @@ package vehiculos;
  */
 public class pruebaVehiculo {
     
+    public static void main (String[] args) {
+        // Instanciación de un objeto
+        // Reserva de espacio en memoria, dando valor a sus atributos
+        
+        vehiculo v1 = new vehiculo("7779KRK", "Bugati", 0);
+        vehiculo v2 = new vehiculo("3333ppp", "Renualt", 100);
+        
+        System.out.println("Matricula v1: " + v1.getMatricula());;
+        System.out.println("Matricula v2: " + v2.getMatricula());
+        
+        System.out.println("La velocidad de v2 es: " + v2.getVelocidad());
+        
+        // Modificamos la velocidad de v2 estableciendo 200kmh/h
+        
+        v2.setVelocidad(200);
+        
+        System.out.println("Velocidad v2: " + v2.getVelocidad());
+    }
 }
--- src/main/java/vehiculos/vehiculo.java
+++ src/main/java/vehiculos/vehiculo.java
@@ -14,9 +14,10 @@ public class vehiculo {
     
     // Dentro de una clase hay 2 cosas: Atributos y Metodos
     // Atributos - características 
-    String matricula;
-    String marca;
-    double velocidad;
+    // Atributos encapsulados --> getter / setter
+    private String matricula;
+    private String marca;
+    private double velocidad;
     
     // Métodos - acciones
     // Método constructor, se llama igual que la clase, siempre es publico
@@ -29,5 +30,79 @@ public class vehiculo {
         
         
     }
+    // Getter y Setter son public
+    //Métodos get --> sirven para obtener datos
     
+    public String getMatricula() {
+        return this.matricula;
+    }
+    
+    public String getMarca() {
+        return this.marca;
+    }
+    
+    public double getVelocidad () {
+        return this.velocidad;
+    }
+    
+    // Método set--> Sirven para establecer datos en los atributos
+    
+    public void setMatricula(String matricula) {
+        this.matricula=matricula;
+    }
+    
+    public void setMarca(String marca) {
+        this.marca=marca;
+    }
+    
+    public void setVelocidad (double velocidad){
+        this.velocidad = velocidad;
+    }
+    
+    
+    /** PARA HACERLO DE FORMA AUTOMATICA
+     * 
+     * // Insert code -- getter y setter -- encapsulate para hacerlos privados
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    // asi muestra todos los datos
+    //@Override
+    //public String toString() {
+        //return "vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", velocidad=" + velocidad + '}';
+        
+        // Es preferible use string builder
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("vehiculo{");
+        sb.append("matricula=").append(matricula);
+        sb.append(", marca=").append(marca);
+        sb.append(", velocidad=").append(velocidad);
+        sb.append('}');
+        return sb.toString();
+    }
+     */
 }
